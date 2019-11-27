5bd57e245c6d3571064f48b2ed7ca0d78cdf5de9
Mateusz <you@example.com>
2019-11-27 10:49:20 +0100

    fsdfsd

--- Dziendobry/src/dziendobry/Main.java
+++ Dziendobry/src/dziendobry/Main.java
@@ -5,6 +5,16 @@
  */
 package dziendobry;
 
+import static dziendobry.zliczanie.numberOFWords;
+import static dziendobry.zliczanie.numberOfLines;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.LineNumberReader;
+import java.util.Scanner;
+
 /**
  *
  * @author student
@@ -15,7 +25,21 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+    try {
+        System.out.println("Podaj ścieżkę dostępu do pliku: ");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String sciezka = br.readLine();
+        FileReader in = new FileReader(sciezka);
+        System.out.println("Ilość linii: " + numberOfLines(in));
+        System.out.println("Liczba slów: " + numberOFWords(in));
+        in.close();
+        br.close();
+
+    } catch (FileNotFoundException e) {
+        System.out.println("Plik nie istnieje");
+        System.exit(1);
+    } catch (IOException ex) {
+        ex.printStackTrace();
     }
-    
+}
 }
