2ddab5390374f834df287ba0f0055a54224513d5
Jakub <jakub@gmail.com>
2019-11-27 10:37:51 +0100

    Task 5

--- Dziendobry/src/dziendobry/Main.java
+++ Dziendobry/src/dziendobry/Main.java
@@ -5,6 +5,8 @@
  */
 package dziendobry;
 
+import java.util.Scanner;
+
 /**
  *
  * @author student
@@ -16,6 +18,10 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.print("Podaj liczbe pliska: ");
+        Scanner sc = new Scanner(System.in);
+        int liczba = sc.nextInt();
+        System.out.println("Twoja liczba to " + liczba);
     }
     
 }
