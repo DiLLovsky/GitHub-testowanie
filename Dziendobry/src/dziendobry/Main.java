700701dbd385b0eb1f5d24d5d016b4e99a3762f4
Jakub <jakub@gmail.com>
2019-11-27 11:08:28 +0100

    Task 6

--- Dziendobry/src/dziendobry/Main.java
+++ Dziendobry/src/dziendobry/Main.java
@@ -21,7 +21,7 @@ public class Main {
         Scanner scan = new Scanner(System.in);
         System.out.print("We coś napisz: ");
         String napis = scan.nextLine();
-        napis = napis.replaceAll(" ", "*").toLowerCase();
+        napis = napis.replaceAll(" ", "Dawid Debil").toLowerCase();
         System.out.println(napis);
         
         
